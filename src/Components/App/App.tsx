581c6b600ecc9fe8ccafe6c7b82517591babe5ee
Devin Haynes <tidalsites@gmail.com>
2021-09-27 12:07:18 -0400

    Initial commit

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import "./App.scss";
+import { Home } from "../Home/Home";
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+};
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import "./Home.scss";
+
+export const Home: FC = () => {
+  return <div className="Home">A to B Towing</div>;
+};
