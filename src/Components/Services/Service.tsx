6a01c9a40066607852fe9183a15defc5cde82152
Devin Haynes <tidalsites@gmail.com>
2021-10-04 23:30:18 -0400

    Added all new icons

--- src/Components/Services/Service.tsx
+++ src/Components/Services/Service.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import tow from "../../Assets/tow-icon.png";
 import tire from "../../Assets/tire-icon.png";
+import battery from "../../Assets/battery-icon.png";
+import gas from "../../Assets/gas-icon.png";
+import unlock from "../../Assets/unlock-icon.png";
 
 interface IServiceContent {
   text: string;
@@ -15,10 +18,19 @@ interface IService {
 }
 
 export const Service: FC<IService> = ({ name, content, icon }) => {
-  let IconComponent = () => <img src={tow} alt="Tow truck" />;
+  let IconComponent: FC = TowIcon;
   switch (icon) {
     case "tire":
-      IconComponent = () => <img src={tire} alt="Flat tire" />;
+      IconComponent = TireIcon;
+      break;
+    case "battery":
+      IconComponent = BatteryIcon;
+      break;
+    case "gas":
+      IconComponent = GasIcon;
+      break;
+    case "unlock":
+      IconComponent = UnlockIcon;
       break;
     default:
       break;
@@ -27,7 +39,6 @@ export const Service: FC<IService> = ({ name, content, icon }) => {
   return (
     <div className="Services__content__service Service">
       <div className="Services__content__service__icon">
-        {/* <img src={tow} alt="Tow truck" /> */}
         <IconComponent />
       </div>
       <div className="Services__content__service__name">{name}</div>
@@ -46,4 +57,20 @@ export const Service: FC<IService> = ({ name, content, icon }) => {
 
 const TowIcon: FC = () => {
   return <img src={tow} alt="Tow Truck" />;
+};
+
+const TireIcon: FC = () => {
+  return <img src={tire} alt="Flat tire" />;
+};
+
+const BatteryIcon: FC = () => {
+  return <img src={battery} alt="Car battery" />;
+};
+
+const GasIcon: FC = () => {
+  return <img src={gas} alt="Gas container" />;
+};
+
+const UnlockIcon: FC = () => {
+  return <img src={unlock} alt="Car with an unlocked padlock" />;
 };
